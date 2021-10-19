2399c299ce3a2b18faf5aed402dc5c10965aa66e
SurukatiAsutoshDora <Ashish8280232017>
2021-10-19 13:14:17 +0530

    second commit

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.post("/failure",function(req,res){
 
 
 
-app.listen(process.env.PORT || 3000 ,function()
+app.listen(process.env.PORT || 3000,function()
 {
   console.log("server is running on port 3000");
 });
